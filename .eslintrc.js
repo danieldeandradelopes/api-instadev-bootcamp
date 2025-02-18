e0b72dfe5387648eace75e9b6c5a42bb01e7843f
NatanCedroDev <natancedrodev@gmail.com>
2025-02-18 15:35:31 -0300

    estudos

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env:{
+        commonjs: true,
+        es2021: true,
+        node: true,
+    }, 
+    extends: [ 
+        'airbnb-base' ,
+    ],
+    parserOptions: {
+        ecmaVersion: 12, 
+    },
+    rules: {
+        'no-params-reassign': 'off' , 
+        camelcase: 'off' , 
+        'no-usuned-vars': ['error', {argsIgnorePattern: 'next' }],
+    },
+};
