3135e2c852ebdc2081bc5ff961b67a3e62760901
Daniel Lopes <danieldeandradelopes@gmail.com>
2025-06-06 19:12:43 -0300

    ignore

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-dialect: process.env.DIALECT,
-host: process.env.HOST,
-username: process.env.DB_USERNAME,
-password: process.env.PASSWORD,
-database: process.env.DATABASE,
-port: process.env.DB_PORT,
-define: {
-    timestamps:true,
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
     underscored: true,
-    underscoredAll:true,
-},
+    underscoredAll: true,
+  },
 };
-
